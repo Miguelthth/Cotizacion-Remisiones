7564c32cbdaca802993ca6c890f9ed58434a9386
Miguelthth <miguelth9316@gmail.com>
2026-09-11 18:05:10 -0700

    y

--- COTIZACION-REMISION/sw.js
+++ COTIZACION-REMISION/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sumetec-rem-70e6fa0f3f';
+const CACHE = 'sumetec-rem-e417d9deb7';
 const PREFIJO = 'sumetec-rem-';
 // Assets pesados (jsPDF ~400 KB) en su PROPIA caché, versionada aparte del shell.
 // Antes vivían dentro de CACHE: como ese nombre es un hash del shell, cambiar una
--- COTIZACION-REMISION/version.js
+++ COTIZACION-REMISION/version.js
@@ -1,3 +1,3 @@
 // Generado por build_deploy.py -- no editar.
-const VERSION_DEPLOY = '2026-09-12T00:55:28Z';
-const VERSION_CODIGO = '70e6fa0f3f';
+const VERSION_DEPLOY = '2026-09-12T01:04:37Z';
+const VERSION_CODIGO = 'e417d9deb7';
